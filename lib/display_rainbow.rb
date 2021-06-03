b417fcb8d2dbee50667cd7aef449e36cccefe688
Garren Goudy <garren.goudy@yahoo.com>
2021-06-03 13:57:09 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,11 +1,3 @@
 # Write your #display_rainbow method here
 
 def display_rainbow
-  print "red"
-  print "orange"
-  print "yellow"
-  print "green"
-  print "blue"
-  print "indigo"
-  print "violet"
-end
